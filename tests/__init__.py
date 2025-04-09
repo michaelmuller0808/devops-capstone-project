d39507a88d4296c77938fd260a47df5f6ef39bad
michaelmuller0808 <141016999+michaelmuller0808@users.noreply.github.com>
2025-04-09 13:49:47 +0800

    Update __init__.py

--- tests/__init__.py
+++ tests/__init__.py
@@ -2,5 +2,5 @@ from flask import Flask
 from flask_talisman import Talisman
 from flask_cors import CORS
 app = Flask(__name__)
-Talisman.init_app(app, content_security_policy=None)
+talisman = Talisman(app, content_security_policy="default-src 'self';")
 CORS(app, resources={r"/*": {"origins": "*"}})
