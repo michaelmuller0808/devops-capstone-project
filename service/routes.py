29f9a9db4cff954cc3a3830d054858809c45f2fa
michaelmuller0808 <141016999+michaelmuller0808@users.noreply.github.com>
2025-04-09 13:33:33 +0800

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -194,4 +194,3 @@ def test_method_not_allowed(self):
     """It should not allow an illegal method call"""
     resp = self.client.delete(BASE_URL)
     self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
-    
