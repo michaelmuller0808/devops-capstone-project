80df234f2cf8dc0e8f8fffd8d50d2f16beac9d6b
michaelmuller0808 <141016999+michaelmuller0808@users.noreply.github.com>
2025-04-09 13:26:13 +0800

    Update routes.py

--- service/routes.py
+++ service/routes.py
@@ -57,6 +57,7 @@ def create_accounts():
         jsonify(message), status.HTTP_201_CREATED, {"Location": location_url}
     )
 
+
 ######################################################################
 # LIST ALL ACCOUNTS
 ######################################################################
@@ -148,10 +149,12 @@ def check_content_type(media_type):
         f"Content-Type must be {media_type}",
     )
 
+
 def test_get_account_not_found(self):
     """It should not Read an Account that is not found"""
     resp = self.client.get(f"{BASE_URL}/0")
     self.assertEqual(resp.status_code, status.HTTP_404_NOT_FOUND)
+
 
 def test_get_account_list(self):
     """It should Get a list of Accounts"""
@@ -160,6 +163,7 @@ def test_get_account_list(self):
     self.assertEqual(resp.status_code, status.HTTP_200_OK)
     data = resp.get_json()
     self.assertEqual(len(data), 5)
+
 
 def test_update_account(self):
     """It should Update an existing Account"""
@@ -174,7 +178,8 @@ def test_update_account(self):
     resp = self.client.put(f"{BASE_URL}/{new_account['id']}", json=new_account)
     self.assertEqual(resp.status_code, status.HTTP_200_OK)
     updated_account = resp.get_json()
-    self.assertEqual(updated_account["name"], status.HTTP_200_OK)
+    self.assertEqual(updated_account["name"], "Something Known")
+
 
 def test_delete_account(self):
     """It should Delete an Account"""
@@ -182,7 +187,9 @@ def test_delete_account(self):
     resp = self.client.delete(f"{BASE_URL}/{account.id}")
     self.assertEqual(resp.status_code, status.HTTP_204_NO_CONTENT)
 
+
 def test_method_not_allowed(self):
     """It should not allow an illegal method call"""
     resp = self.client.delete(BASE_URL)
     self.assertEqual(resp.status_code, status.HTTP_405_METHOD_NOT_ALLOWED)
+
